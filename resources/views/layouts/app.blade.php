ed8b02e8d586614fb07e521cc89eb8041c65d945
keepthinkingit <781130764@qq.com>
2019-03-04 00:39:50 +0800

    add blade layouts and install bulma

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,26 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="./favicon.png" type="image/x-icon">
     <link rel="apple-touch-icon" href="./favicon.png">
-    {{--<!-- CSRF Token -->--}}
-    {{--<meta name="csrf-token" content="{{ csrf_token() }}">--}}
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'laravue-nav') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('css')
-<!-- Fonts -->
-    {{--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">--}}
 
 <!-- Styles -->
 </head>
 <body>
 <div id="app">
-    <img src="{{ asset('svg/top.svg') }}" alt="top" class="scroll-top">
-    @include('layouts.nav')
+    {{--<img src="{{ asset('svg/top.svg') }}" alt="top" class="scroll-top">--}}
+    {{--@include('layouts.nav')--}}
     This is how we live!
     @yield('content')
     @yield('_footer')
